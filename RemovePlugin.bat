b831deae897f7bd2f12e3d122e335b20b0b4bcda
fdcumt <1040183251@qq.com>
2017-09-30 11:32:01 +0800

    需要设置UE4PATH,引擎路径

--- RemovePlugin.bat
+++ RemovePlugin.bat
@@ -4,5 +4,4 @@ rmdir /s/q %CurDir%Plugins\LuaPlugin\Intermediate
 set UE4PATH=E:\GitHub\MaYunAthena\Athena\UnrealEngine
 
 "%UE4PATH%\Engine\Build\BatchFiles\GenerateProjectFiles.bat" %CurDir%MyProject.uproject
-
 Pause
