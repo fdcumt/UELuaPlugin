6cb885db16dccd7c84aeecc7ad8b7ed93da37680
fdcumt <1040183251@qq.com>
2017-09-30 11:31:02 +0800

    添加window下bat,删除luaPlugin,并且重新generate

---
+++ RemovePlugin.bat
@@ -0,0 +1,8 @@
+set CurDir=%~dp0
+rmdir /s/q %CurDir%Plugins\LuaPlugin\Binaries
+rmdir /s/q %CurDir%Plugins\LuaPlugin\Intermediate
+set UE4PATH=E:\GitHub\MaYunAthena\Athena\UnrealEngine
+
+"%UE4PATH%\Engine\Build\BatchFiles\GenerateProjectFiles.bat" %CurDir%MyProject.uproject
+
+Pause
