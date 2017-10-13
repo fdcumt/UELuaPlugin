c64edd61314aeb256f1378c13ca1ed3c206f20a7
fdcumt <1040183251@qq.com>
2017-10-13 17:13:01 +0800

    整理代码

--- Plugins/LuaPlugin/Source/LuaGenerator/Private/LuaConfigManager.cpp
+++ Plugins/LuaPlugin/Source/LuaGenerator/Private/LuaConfigManager.cpp
@@ -22,19 +22,23 @@ void FLuaConfigManager::Init()
 	GConfig->GetArray(TEXT("ConfigClassFiles"), TEXT("ConfigClassFileName"), ClassConfigFileNames, ConfigFilePath);
 	GConfig->GetArray(TEXT("AdditionalIncludeHeaders"), TEXT("IncludeHeader"), AdditionalIncludeHeaders, ConfigFilePath);
 
-	if (FPaths::IsProjectFilePathSet())
+	if (!FPaths::IsProjectFilePathSet())
 	{
-		FConfigFile* File = GConfig->Find(ProjectPath / LuaConfigFileRelativePath, false);
-		if (File != nullptr)
-		{
-			FProjectDescriptor ProjectDescriptor;
-			FText OutError;
-
-			// FPaths::GetProjectFilePath() return the  path of project's .uproject file
-			ProjectDescriptor.Load(ProjectFilePath, OutError);
-
-			// init the GameModuleName by the first module name in the file of ProjectName.uproject 
-			ProjectDescriptor.Modules[0].Name.ToString(GameModuleName);
-		}
+		DebugLog(TEXT("Project file path not set!"));
 	}
+
+	FConfigFile* File = GConfig->Find(ProjectPath/LuaConfigFileRelativePath, false);
+	if (File == nullptr)
+	{
+		DebugLog(TEXT("not found config file %s!"), *(ProjectPath / LuaConfigFileRelativePath));
+	}
+
+	FText OutError;
+	FProjectDescriptor ProjectDescriptor;
+
+	// FPaths::GetProjectFilePath() return the  path of project's .uproject file
+	ProjectDescriptor.Load(ProjectFilePath, OutError);
+
+	// init the GameModuleName by the first module name in the file of ProjectName.uproject 
+	ProjectDescriptor.Modules[0].Name.ToString(GameModuleName);
 }
